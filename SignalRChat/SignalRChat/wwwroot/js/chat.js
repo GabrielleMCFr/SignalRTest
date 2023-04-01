e50066a61cb068070987c71ac7629fae2d5be793
Gabrielle Marsallon <gabrielle.mc@outlook.fr>
2023-04-01 20:30:58 +0200

    tuto signalR

---
+++ SignalRChat/SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,35 @@
+﻿
+"use strict";
+
+// create a connection
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagessList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+
+    li.textContent = `${user} says ${message}`;
+});
+
+// enable the submit button when the connection has started
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+// send a message when the submit button is clicked on
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
